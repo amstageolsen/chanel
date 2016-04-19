62503fd17abaaee2486af8df797f700d2805b079
Anne Mette Stage Olsen <ams@pentia.dk>
2016-04-19 20:43:39 +0200

    Sjov besked til Lars i Console log

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+console.log('%c Hvad siger du til det, Lars? :-)','font-size:70px; background: red; color: #fff;');
+
 $(".menu-collapsed").click(function() {
-  $(this).toggleClass("menu-expanded");
+    $(this).toggleClass("menu-expanded");
 });
